b12b21d8a7b0efeebf3eb42eb897e8111773e76b
carrotdy <83288448+carrotdy@users.noreply.github.com>
2023-10-30 01:47:05 +0900

    복지몰 무한스크롤 구현

--- src/views/RoutePath.tsx
+++ src/views/RoutePath.tsx
@@ -4,30 +4,35 @@ import 회사소개 from "./main/회사소개";
 import 채용상세 from "./main/채용_상세";
 import 복지몰 from "./main/복지몰";
 import 장바구니 from "./main/장바구니";
+import 게시판 from "./main/게시판";
 
 export class RoutePath {
-    static readonly 회사소개 = {
-        path: "/",
-        element: <회사소개 />
-    };
-    static readonly 팀문화 = {
-        path: "/views/team-culture",
-        element: <팀문화 />
-    };
-    static readonly 채용 = {
-        path: "/views/careers",
-        element: <채용 />
-    };
-    static readonly 채용상세 = {
-        path: "/views/careerDetail",
-        element: <채용상세 />
-    };
-    static readonly 복지몰 = {
-        path: "/views/welfare",
-        element: <복지몰 />
-    };
-    static readonly 장바구니 = {
-        path: "/views/cart",
-        element: <장바구니 />
-    };
+  static readonly 회사소개 = {
+    path: "/",
+    element: <회사소개 />,
+  };
+  static readonly 팀문화 = {
+    path: "/views/team-culture",
+    element: <팀문화 />,
+  };
+  static readonly 채용 = {
+    path: "/views/careers",
+    element: <채용 />,
+  };
+  static readonly 채용상세 = {
+    path: "/views/careerDetail",
+    element: <채용상세 />,
+  };
+  static readonly 복지몰 = {
+    path: "/views/welfare",
+    element: <복지몰 />,
+  };
+  static readonly 장바구니 = {
+    path: "/views/cart",
+    element: <장바구니 />,
+  };
+  static readonly 게시판 = {
+    path: "/views/notice",
+    element: <게시판 />,
+  };
 }
--- src/views/main/복지몰.tsx
+++ src/views/main/복지몰.tsx
@@ -2,19 +2,59 @@ import styled from "styled-components";
 import { Container, SubTitle, Title } from "../components/Common";
 import DummyData from "../constants/data/DummyData";
 import 복지몰_상품 from "./복지몰_상품";
+import { useEffect, useState } from "react";
+import { SyncLoader } from "react-spinners";
+import { Color } from "../constants/style/Color";
+import { debounce } from "lodash-es";
+
+const ITEMS_PER_PAGE = 6;
 
 const 복지몰: React.FunctionComponent = () => {
+  const [visibleItems, setVisibleItems] = useState<number>(ITEMS_PER_PAGE); // 페이지당 보여줄 항목 수
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  //무한스크롤
+  const handleScroll = () => {
+    if (isLoading) return;
+
+    const scrollHeight = document.documentElement.scrollHeight;
+    const currentHeight =
+      document.documentElement.scrollTop + window.innerHeight;
+
+    if (currentHeight + 1 >= scrollHeight) {
+      setIsLoading(true);
+      setTimeout(() => {
+        setVisibleItems(visibleItems + ITEMS_PER_PAGE);
+        setIsLoading(false);
+      }, 1000);
+    }
+  };
+
+  const debouncedHandleScroll = debounce(handleScroll, 100);
+
+  useEffect(() => {
+    window.addEventListener("scroll", debouncedHandleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [visibleItems]);
+
   return (
     <Container>
       <Title>쇼핑모아 복지몰</Title>
       <SubTitle>
-        쇼핑모아 모든 직원들이 복지포인트를 이용하여<br />쇼핑을 할 수 있는 공간입니다
+        쇼핑모아 모든 직원들이 복지포인트를 이용하여
+        <br />
+        쇼핑을 할 수 있는 공간입니다
       </SubTitle>
       <ProductCardContainer>
-        {DummyData.map((data, index) => {
+        {DummyData.slice(0, visibleItems).map((data, index) => {
           return <복지몰_상품 key={data.id} data={data} index={index} />;
         })}
       </ProductCardContainer>
+      {isLoading && (
+        <div style={{ textAlign: "center", margin: "10px 0" }}>
+          <SyncLoader color={Color.Orange} />
+        </div>
+      )}
     </Container>
   );
 };
