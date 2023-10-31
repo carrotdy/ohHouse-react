9256b4fc075e08359bd1f4a67ed49226e02c696d
carrotdy <83288448+carrotdy@users.noreply.github.com>
2023-10-31 16:37:51 +0900

    footer 내용 변경

--- src/views/common/Footer.tsx
+++ src/views/common/Footer.tsx
@@ -8,8 +8,8 @@ export const Footer: React.FunctionComponent = () => {
     <>
       <BorderTopLineGray30 />
       <FooterContainer>
-        <Title>(주)마켓모아</Title>
-        <Contents>대표이사: 이승재</Contents>
+        <Title>(주)오하우스</Title>
+        <Contents>대표이사: 홍다영</Contents>
         <Contents>
           주소 : 서울특별시 강남구 강남대로7 |{" "}
           <span>문의전화 : 02-000-0000 | 팩스 : 02-000-0000</span>
