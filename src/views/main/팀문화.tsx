e6b0cdcbe1ff36b4fb47122c399462c4477aba9b
carrotdy <83288448+carrotdy@users.noreply.github.com>
2023-11-06 15:28:52 +0900

    팀문화 완료

--- src/views/main/팀문화.tsx
+++ src/views/main/팀문화.tsx
@@ -12,22 +12,26 @@ const 팀문화: React.FunctionComponent = () => {
   return (
     <Container>
       <팀문화_직원의소리 />
-      <Title style={{ textAlign: "center", marginBottom: "30px" }}>
+      <Title
+        data-aos="fade-up"
+        style={{ textAlign: "center", marginBottom: "30px" }}
+      >
         더 나은 오늘을 만들어 가는 당신을
         <br />
         아낌없이 지원합니다
       </Title>
-      <div className="team-culture-content-container">
+      <div data-aos="fade-up" className="team-culture-content-container">
         {data.map((data, i) => {
           return (
             <div key={i}>
               <h1>{data.id}</h1>
-              <h2>{data.title}</h2>
+              <h2 data-aos="fade-up">{data.title}</h2>
               <div
                 className="team-culture-content"
                 dangerouslySetInnerHTML={{
                   __html: data.content,
                 }}
+                data-aos="fade-up"
               />
             </div>
           );
