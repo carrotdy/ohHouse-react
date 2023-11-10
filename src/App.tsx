2ce013625851c82eb6ea1132a9ff1abe08bdfb44
carrotdy <83288448+carrotdy@users.noreply.github.com>
2023-11-10 15:27:47 +0900

    minWidth 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Main from "./Main";
+import { ScrollToTop } from "./components/Common";
 import { AuthProvider } from "./context/AuthProvider";
 import { Layout } from "./layouts/Layout";
-import { ScrollToTop } from "./components/Common";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 AOS.init({
   duration: 1000,
