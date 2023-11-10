2cf5e45b9ca37668555c383fe15edf460dc21722
carrotdy <83288448+carrotdy@users.noreply.github.com>
2023-11-10 15:48:01 +0900

    불필요한 파일 삭제

--- src/utils/CssUtil.ts
+++ src/utils/CssUtil.ts
@@ -1,30 +1,22 @@
-import { ICssProps } from "./interface";
+export interface ICssProps {
+	[index: string]: string | number | { [index: string]: string | number };
+}
 
 const Media = (maxWidthSize: number, property: ICssProps) => {
-    const mediaQuery = `@media(max-width: ${maxWidthSize}px)`;
+	const mediaQuery = `@media(max-width: ${maxWidthSize}px)`;
 
-    const result: any = {
-    };
-    result[mediaQuery] = {
-        ...property,
-    };
-    return result;
+	const result: any = {
+	};
+	result[mediaQuery] = {
+		...property,
+	};
+	return result;
 };
 const Mobile = (property: ICssProps) => Media(768, property);
 const Tablet = (property: ICssProps) => Media(1150, property);
 
-// flex box
-const FlexContainer = <T>(property: T) => ({
-    display: "flex",
-    ...property,
-});
-const FlexCenterContainer = <T>(property: T) => FlexContainer({
-    justifyContent: "center", alignItems: "center", ...property,
-});
-
 export {
-    Media,
-    Mobile,
-    Tablet,
-    FlexCenterContainer
+	Media,
+	Mobile,
+	Tablet
 }
